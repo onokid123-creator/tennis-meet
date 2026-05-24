9dcd60164ae4268a05d00611d950ee0e565d1375
Rowan <mate2nd@naver.com>
2026-05-25 01:48:04 +0900

    Remove unused Android safe area experiment

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-if (/Android/i.test(navigator.userAgent)) {
-  document.documentElement.classList.add('is-android');
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
